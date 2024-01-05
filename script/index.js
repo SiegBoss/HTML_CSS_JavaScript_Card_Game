b551ef2626176d852f8ea47293e8f670e99488c2
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2024-01-05 10:00:59 -0600

    update

--- script/index.js
+++ script/index.js
@@ -1,12 +1,12 @@
 //Declare variables de las cartas | Declare cards variables
-var watermelon = document.getElementById("container-watermelon");
+var watermelon  = document.getElementById("container-watermelon");
 var watermelon2 = document.getElementById("container-watermelon-2");
-var apple = document.getElementById("container-apple");
-var apple2 = document.getElementById("container-apple-2");
-var avocado = document.getElementById("container-avocado");
-var avocado2 = document.getElementById("container-avocado-2");
-var banana = document.getElementById("container-banana");
-var banana2 = document.getElementById("container-banana-2");
+var apple       = document.getElementById("container-apple");
+var apple2      = document.getElementById("container-apple-2");
+var avocado     = document.getElementById("container-avocado");
+var avocado2    = document.getElementById("container-avocado-2");
+var banana      = document.getElementById("container-banana");
+var banana2     = document.getElementById("container-banana-2");
 
 //Declarando las variables de ayuda | Declare help variables
 var images = [watermelon, watermelon2, apple, apple2, avocado, avocado2, banana, banana2];
@@ -20,8 +20,11 @@ function flipImage(val) {
 
     //Si ya se voltearon 2 cartas, no se puede voltear otra | If 2 cards are already flipped, you can't flip another one
     move++;
+
     if (move > 2) {
+
         move = 2;
+
         return;
     }
 
@@ -29,43 +32,59 @@ function flipImage(val) {
     if (move <= 2) {
 
         //Carta Sandia | Watermelon Card
-        if (val == "watermelon_disguise" && fruits[0] == 0) {
+        if (val == "watermelon-disguise" && fruits[0] == 0) {
+
             watermelon.classList.toggle('is-flipped');
             fruits[0]++;
+
         }
-        else if (val == "watermelon_disguise_2" && fruits[1] == 0) {
+        else if (val == "watermelon-disguise-2" && fruits[1] == 0) {
+
             watermelon2.classList.toggle('is-flipped');
             fruits[1]++;
+
         }
 
         //Carta Manzana | Apple Card
-        if (val == "apple_disguise" && fruits[2] == 0) {
+        if (val == "apple-disguise" && fruits[2] == 0) {
+
             apple.classList.toggle('is-flipped');
             fruits[2]++;
+
         }
-        else if (val == "apple_disguise_2" && fruits[3] == 0) {
+        else if (val == "apple-disguise-2" && fruits[3] == 0) {
+
             apple2.classList.toggle('is-flipped');
             fruits[3]++;
+
         }
 
         //Carta Aguacate | Avocado Card
-        if (val == "avocado_disguise" && fruits[4] == 0) {
+        if (val == "avocado-disguise" && fruits[4] == 0) {
+
             avocado.classList.toggle('is-flipped');
             fruits[4]++;
+
         }
-        else if (val == "avocado_disguise_2" && fruits[5] == 0) {
+        else if (val == "avocado-disguise-2" && fruits[5] == 0) {
+
             avocado2.classList.toggle('is-flipped');
             fruits[5]++;
+    
         }
 
         //Carta Platano | Banana Card
-        if (val == "banana_disguise" && fruits[6] == 0) {
+        if (val == "banana-disguise" && fruits[6] == 0) {
+
             banana.classList.toggle('is-flipped');
             fruits[6]++;
+
         }
-        else if (val == "banana_disguise_2" && fruits[7] == 0) {
+        else if (val == "banana-disguise-2" && fruits[7] == 0) {
+
             banana2.classList.toggle('is-flipped');
             fruits[7]++;
+
         }
     }
 
@@ -80,16 +99,22 @@ function flipImage(val) {
             while (x < 8) {
 
                 if (fruits[x] == 1 && fruits[x + 1] == 1) {
+
                     x += 2;
                     continue;
+
                 }
                 else if (fruits[x] == 1 && fruits[x + 1] == 0) {
+
                     fruits[x] = 0;
                     images[x].classList.toggle('is-flipped');
+
                 }
                 else if (fruits[x] == 0 && fruits[x + 1] == 1) {
+
                     fruits[x + 1] = 0;
                     images[x + 1].classList.toggle('is-flipped');
+
                 }
 
                 x += 2;
@@ -104,7 +129,9 @@ function flipImage(val) {
 
     //Si todas las cartas estan volteadas, se reinicia el juego | If all cards are flipped, the game restarts
     if (fruits[0] == 1 && fruits[1] == 1 && fruits[2] == 1 && fruits[3] == 1 && fruits[4] == 1 && fruits[5] == 1 && fruits[6] == 1 && fruits[7] == 1) {
+
         setTimeout(function () {
+
             pulse = 0;
             shuffleImages();
             fruits = [0, 0, 0, 0, 0, 0, 0, 0];
@@ -117,6 +144,7 @@ function flipImage(val) {
             banana.classList.toggle('is-flipped');
             banana2.classList.toggle('is-flipped');
             pulse = 0;
+
         }, 1000);
     }
 }
@@ -133,44 +161,51 @@ function shuffleImages() {
 
     //Posiciones de las cartas | cards positions
     if (screen.width < 600) {
+
         positions = [
-            { left: "20px", top: "10px" },
-            { left: "20px", top: "120px" },
-            { left: "20px", top: "230px" },
-            { left: "20px", top: "340px" },
-            { left: "150px", top: "10px" },
+
+            { left: "20px",  top: "10px"  },
+            { left: "20px",  top: "120px" },
+            { left: "20px",  top: "230px" },
+            { left: "20px",  top: "340px" },
+            { left: "150px", top: "10px"  },
             { left: "150px", top: "120px" },
             { left: "150px", top: "230px" },
             { left: "150px", top: "340px" }
+
         ];
 
     } else {
+
         positions = [
-            { left: "20px", top: "10px" },
-            { left: "160px", top: "10px" },
-            { left: "300px", top: "10px" },
-            { left: "440px", top: "10px" },
-            { left: "20px", top: "200px" },
+
+            { left: "20px",  top: "10px"  },
+            { left: "160px", top: "10px"  },
+            { left: "300px", top: "10px"  },
+            { left: "440px", top: "10px"  },
+            { left: "20px",  top: "200px" },
             { left: "160px", top: "200px" },
             { left: "300px", top: "200px" },
             { left: "440px", top: "200px" }
+
         ];
     }
-
-
 
     //Revuelve las posiciones | shuffle positions
     positions.sort(() => Math.random() - 0.5);
 
     //Asigna las nuevas posiciones | assign new positions
     for (let i = 0; i < 8; i++) {
+
         images[i].style.left = positions[i].left;
         images[i].style.top = positions[i].top;
         images[i].classList.add("transicion");
+
     }
 
     //Voltea las cartas durante 1 segundo | flip the cards for 1 second
     setTimeout(function () {
+
         watermelon.classList.toggle('is-flipped');
         watermelon2.classList.toggle('is-flipped');
         apple.classList.toggle('is-flipped');
@@ -179,9 +214,11 @@ function shuffleImages() {
         avocado2.classList.toggle('is-flipped');
         banana.classList.toggle('is-flipped');
         banana2.classList.toggle('is-flipped');
+
     }, 1000);
 
     //Regresa las cartas a su posicion original | return the cards to their original position
+    
     watermelon.classList.toggle('is-flipped');
     watermelon2.classList.toggle('is-flipped');
     apple.classList.toggle('is-flipped');
